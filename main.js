8167e21b148381c292208f7e67343b802067e467
Osman19702 <50367585+Osman19702@users.noreply.github.com>
2019-09-17 13:49:11 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,116 @@
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+
+// SET ENV
+process.env.NODE_ENV = 'development';
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+let mainWindow;
+let addWindow;
+
+// Listen for app to be ready
+app.on('ready', function(){
+  // Create new window
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+        nodeIntegration: true }
+  });
+  // Load html in window
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'mainWindow.html'),
+    protocol: 'file:',
+    slashes:true
+  }));
+  // Quit app when closed
+  mainWindow.on('closed', function(){
+    app.quit();
+  });
+
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
+});
+
+// Handle add item window
+function createAddWindow(){
+  addWindow = new BrowserWindow({
+          webPreferences: {
+        nodeIntegration: true } ,
+    width: 400,
+    height:500,
+    title:'Add Shopping List Item'
+  });
+  addWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'addWindow.html'),
+    protocol: 'file:',
+    slashes:true
+  }));
+  // Handle garbage collection
+  addWindow.on('close', function(){
+    addWindow = null;
+  });
+}
+
+// Catch item:add
+ipcMain.on('item:add', function(e, item){
+  mainWindow.webContents.send('item:add',item); /* Buradan */
+  
+  //addWindow.close(); 
+  // Still have a reference to addWindow in memory. Need to reclaim memory (Grabage collection)
+  //addWindow = null;
+});
+
+
+// Create menu template
+const mainMenuTemplate =  [
+  // Each object is a dropdown
+
+      {
+        label:'Add Item',
+        click(){
+          createAddWindow();
+        }
+      },
+      {
+        label:'Clear Items',
+        click(){
+          mainWindow.webContents.send('item:clear');
+        }
+      },
+      {
+        label: 'Quit',
+        accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click(){
+          app.quit();
+        }
+      }
+    
+  
+];
+
+// If OSX, add empty object to menu
+if(process.platform == 'darwin'){
+  mainMenuTemplate.unshift({});
+}
+
+// Add developer tools option if in dev
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu:[
+      {
+        role: 'reload'
+      },
+      {
+        label: 'Toggle DevTools',
+        accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      }
+    ]
+  });
+}
